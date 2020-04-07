cb58433d4c2ccf696714aab8418fc7afd5918639
lstest01 <63073909+lstest01@users.noreply.github.com>
2020-04-07 16:38:57 +0800

    Update JenkinsTest01.py

--- JenkinsTest01.py
+++ JenkinsTest01.py
@@ -1,4 +1,5 @@
-﻿for i in range(1,10):
-    for j in range(1,i+1):
-        print(f"{j}*{i}={i*j}",end=" ")
-    print("")
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("http://www.baidu.com")
